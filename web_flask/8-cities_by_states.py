9baf2a1adf43fe9f9030e363d36abea8d49a2891
iheb-drihmi <ihebdrihmi2002@gmail.com>
2023-08-18 15:51:15 +0100

    8-cities_by_states.py

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -10,13 +10,13 @@ app = Flask(__name__)
 
 @app.teardown_appcontext
 def close_db(error):
-    ''' Remove the current SQLAlchemy Session '''
-     storage.close()
+    """ Remove the current SQLAlchemy Session """
+    storage.close()
 
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
-    '''displays a HTML page with a list of states '''
+    """ displays a HTML page with a list of states """
     states = storage.all(State).values()
     states = sorted(states, key=lambda k: k.name)
     return render_template('7-states_list.html', states=states)
@@ -24,7 +24,7 @@ def states_list():
 
 @app.route('/cities_by_states', strict_slashes=False)
 def cities_list():
-   '''displays a HTML page with a list of cities by states '''
+    """ displays a HTML page with a list of cities by states """
     states = storage.all(State).values()
     states = sorted(states, key=lambda k: k.name)
     st_ct = []
@@ -36,5 +36,5 @@ def cities_list():
 
 
 if __name__ == "__main__":
-    ''' Main Function'''
+    """ Main Function """
     app.run(host='0.0.0.0', port=5000)
